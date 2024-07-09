0dd08091b4cbae4863c9a75e75f9ec594e5e7cab
Frandy Slueue <Frandy4ever@gmail.com>
2024-07-09 14:08:11 -0500

    feat: Add root queries for projects and tasks
    - Added tasks and projects fields to RootQueryType to fetch all tasks and projects
    - Updated schema.js to include GraphQLList for tasks and projects queries
    - Created graphiql5 file to test query fetching all projects with their tasks in GraphiQL
    - Enabled GraphiQL interface in app.js for testing GraphQL queries

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,22 +1,15 @@
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLSchema,
-  GraphQLID,
-  GraphQLList
-} = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID, GraphQLList } = require('graphql');
 const _ = require('lodash');
 
-// Dummy data for tasks and projects
-const tasks = [
-  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html ...', projectId: '1' },
-  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html ...', projectId: '1' }
-];
-
+// Dummy data
 const projects = [
   { id: '1', title: 'Advanced HTML', weight: 1, description: 'Welcome to the Web Stack specialization. ...' },
   { id: '2', title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework ...' }
+];
+
+const tasks = [
+  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html ...', projectId: '1' },
+  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html ...', projectId: '1' }
 ];
 
 const TaskType = new GraphQLObjectType({
@@ -66,6 +59,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(projects, { id: args.id });
+      }
+    },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return tasks;
+      }
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
       }
     }
   }
